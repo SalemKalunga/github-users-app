7724f70a25bb6401bafe3fe2c23ca3405816e21a
Salem Kalunga <salemlegrand65@gmail.com>
2023-01-30 22:34:46 +0200

    done with the header design

--- src/components/header.component/header.component.jsx
+++ src/components/header.component/header.component.jsx
@@ -1,8 +1,26 @@
 import "./header.styles.scss";
+import logo from "../../assets/logoCopy.png";
 const Header = () => {
   return (
     <header>
-      <h1>hello header</h1>
+      <nav>
+        <div className="logo-container">
+          <img src={logo} alt="logo" width={100} />
+        </div>
+        <div className="links-container">
+          <ul>
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li>
+              <a href="#">SignUp</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 };
